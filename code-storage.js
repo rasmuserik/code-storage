96c22a3b91dc29a7bc4146b3ca49ba7fda92d893
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-02-22 12:13:23 +0100

    /hash/ endpoint

--- code-storage.js
+++ code-storage.js
@@ -4,41 +4,47 @@ var sha256 = require('js-sha256');
 
 var time = 0;
 server = require('http').createServer((req, res) => {
-  if(req.method === 'POST') {
-    var data = '';
-    req.on('data', chunk => data += chunk.toString());
-    req.on('end', () => {
-      if(data.length > 60000) {
-        console.log('big data.length', data.length, id);
-        res.statusCode = 413;
-        res.end();
-      } else {
-        var id = sha256(data).slice(0,24);
-        db.put(id, data, err => {
-          if(err) {
-            console.log(err);
-            res.end();
-          }
-          res.end(id);
-        });
-      }
-    });
-  } else {
-    if(req.url === '/icon.png' || req.url === '/README.md' || req.url === '/index.html') {
-      res.end(require('fs').readFileSync(req.url.slice(1)));
-      return;
+
+  if(req.url === '/icon.png' || req.url === '/README.md' || req.url === '/index.html') {
+    res.end(require('fs').readFileSync(req.url.slice(1)));
+    return;
+
+  } else if(req.url.startsWith('/hash')) {
+    if(req.method === 'POST') {
+      var data = '';
+      req.on('data', chunk => data += chunk.toString());
+      req.on('end', () => {
+        if(data.length > 60000) {
+          console.log('big data.length', data.length, id);
+          res.statusCode = 413;
+          res.end();
+        } else {
+          var id =  sha256(data).slice(0,24);
+          db.put(id, data, err => {
+            if(err) {
+              console.log(err);
+              res.end();
+            }
+            res.end(id);
+          });
+        }
+      });
+    } else {
+      var id = req.url.slice(6);
+      db.get(id, (err, val) => {
+        if(err) {
+          console.log(err);
+          res.statusCode = 404;
+          res.end();
+        } else {
+          res.setHeader('Cache-Control', 'public, max-age=31557600');
+          res.end(val);
+        }
+      });
     }
-    var id = req.url.slice(1);
-    db.get(id, (err, val) => {
-      if(err) {
-        console.log(err);
-        res.statusCode = 404;
-        res.end();
-      } else {
-        res.setHeader('Cache-Control', 'public, max-age=31557600');
-        res.end(val);
-      }
-    });
+  } else {
+    res.statusCode = 404;
+    res.end('');
   }
 });
 
--- dev.sh
+++ dev.sh
@@ -3,8 +3,8 @@ while inotifywait -e modify,close_write,move_self -q *.js *.html
 do 
   kill `cat .pid`
   sleep 0.1
-  (sleep 0.5; curl -X PUT -d 'console.log("hi");' http://localhost:8888/iyx8muyi0z8kflt ) &
-  (sleep 1; curl http://localhost:8888/iyx8muyi0z8kflt ) &
+  (sleep 0.5; curl -X POST -d 'console.log("hi");' http://localhost:8888/hash ) &
+  (sleep 1; curl http://localhost:8888/hash/6327935c4103bdd527e12104) &
   node code-storage.js $@ &
   echo $! > .pid
   sleep 3
